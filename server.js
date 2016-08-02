a231790b51b8f846c7c3e29d5d00afbb7d9c3f04
Daniel Glaser <glaserd92@gmail.com>
2016-08-02 10:36:02 -0500

    Fix server route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var server = express();
 
-server.use(express.static(__dirname + '/client/title.html'));
+server.use(express.static(__dirname + '/client'));
 
 server.listen(process.env.PORT || 3000);
